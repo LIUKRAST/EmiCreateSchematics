cfc8bd42e5090966cce9600bc1f763d6ac860656
LiukRast <darthlucavader@gmail.com>
2024-08-12 19:30:03 +0200

    Fixed Emi rendering bug

--- src/main/java/net/frozenblock/liukrast/schematicdisplay/mixin/ItemEmiStackMixin.java
+++ src/main/java/net/frozenblock/liukrast/schematicdisplay/mixin/ItemEmiStackMixin.java
@@ -5,6 +5,7 @@ import dev.emi.emi.EmiPort;
 import dev.emi.emi.EmiRenderHelper;
 import dev.emi.emi.api.stack.ItemEmiStack;
 import dev.emi.emi.runtime.EmiDrawContext;
+import net.minecraft.client.gui.GuiGraphics;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -14,7 +15,7 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 @Mixin(ItemEmiStack.class)
 public final class ItemEmiStackMixin {
     @Inject(at = @At("TAIL"), method = "render", remap = false)
-    private void render(final PoseStack matrices,
+    private void render(final GuiGraphics matrices,
                         final int x, final int y,
                         final float delta,
                         final int flags,
