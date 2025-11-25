9ce5904a50ecd331407a71e3c6449f8ce75d87e5
swzo <omarahmedmmsalem@gmail.com>
2025-11-25 17:42:48 +0200

    sort items when transfering to clipboard by their count

--- src/main/java/net/liukrast/schematicdisplay/clipboard/ClipboardScreenUtils.java
+++ src/main/java/net/liukrast/schematicdisplay/clipboard/ClipboardScreenUtils.java
@@ -7,6 +7,8 @@ import net.minecraft.core.component.DataComponents;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.component.CustomData;
 
+import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 
 public final class ClipboardScreenUtils {
@@ -14,18 +16,20 @@ public final class ClipboardScreenUtils {
     private ClipboardScreenUtils() {}
 
     public static boolean load(List<List<ClipboardEntry>> pages, final boolean save) {
-        for(var page : pages) {
-            for(var entry : page) {
-                if(save) {
-                    ItemStack stack1 = entry.icon.copy();
-                    stack1.update(DataComponents.CUSTOM_DATA, CustomData.EMPTY, existingData ->
-                            existingData.update(tag -> tag.putLong(REAL_COUNT_KEY, entry.itemAmount))
-                    );
-                    EmiStack emiStack = EmiStack.of(stack1);
-                    emiStack.setAmount(1);
-                    EmiFavorites.addFavorite(emiStack);
-                }
-            }
+        if (!save) return true;
+        List<ClipboardEntry> allEntries = new ArrayList<>();
+        for(var page : pages)
+            allEntries.addAll(page);
+        allEntries.sort(Comparator.comparingInt((ClipboardEntry entry) -> entry.itemAmount).reversed());
+
+        for(var entry : allEntries) {
+            ItemStack stack1 = entry.icon.copy();
+            stack1.update(DataComponents.CUSTOM_DATA, CustomData.EMPTY, existingData ->
+                    existingData.update(tag -> tag.putLong(REAL_COUNT_KEY, entry.itemAmount))
+            );
+            EmiStack emiStack = EmiStack.of(stack1);
+            emiStack.setAmount(1);
+            EmiFavorites.addFavorite(emiStack);
         }
         return true;
     }
